9b39aa11439547b630638e61be38095b67c85f4a
candyPz <candy_pz@outlook.com>
2018-01-19 23:53:04 +0800

    增加 多语言

---
+++ Assets/Script/Utility/Localize.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ET {
+    public class Localize {
+        private static readonly Localize m_instance = new Localize();
+
+        private string m_language = "";
+        private Dictionary<string, Dictionary<string, string>> m_dictionary;
+
+        public Localize() {
+            m_dictionary = new Dictionary<string, Dictionary<string, string>> ();
+        }
+
+        public static Localize getInstance() {
+            return m_instance;
+        }
+
+        public string language {
+            set {
+                if (m_language != value) {
+                    m_language = value;
+                }
+            }
+            get {
+                return m_language;
+            }
+        }
+
+        public string value(string key) {
+            if (m_dictionary[key] != null) {
+                if (string.IsNullOrEmpty(m_dictionary[key][m_language])) {
+                    return m_dictionary[key][m_language];
+                }
+            }
+            return key;
+        }
+
+    }
+}
---
+++ Assets/Script/Utility/UGUILocalize.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ET {
+    public class UGUILocalize : MonoBehaviour {
+        public string key;
+
+        public string value {
+            set {
+                if (string.IsNullOrEmpty(value)) {
+                    var _uguiText = GetComponent<Text>();
+                    if (_uguiText != null) {
+                        _uguiText.text = value;
+                    }
+                }
+            }
+        }
+
+        void OnLocalize() {
+            if (string.IsNullOrEmpty(key)) {
+                var _uguiText = GetComponent<Text>();
+                if (_uguiText != null) {
+                    key = _uguiText.text;
+                }
+            }
+            if (!string.IsNullOrEmpty(key)) {
+                value = "";
+            }
+        }
+
+        void Start() {
+            OnLocalize();
+        }
+    }
+}
